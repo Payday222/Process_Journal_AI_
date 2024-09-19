4c2612e84cd4e62b76c1f679ebb40b7047879bde
William <michalkaliszuk7@gmail.com>
2024-09-19 22:22:59 +0200

    Started adding save mechanism. Reworked reward logic
    Fixed a few logic issues

--- myAgents.cs
+++ myAgents.cs
@@ -1,8 +1,10 @@
+using System;
 using System.Data.SqlTypes;
 using UnityEngine;
 using Unity.MLAgents;
 using Unity.MLAgents.Sensors;
 using Unity.MLAgents.Actuators;
+using System.IO;
 public class myAgent : Agent
 {
         [Header("RESULTS")]
@@ -10,23 +12,24 @@ public class myAgent : Agent
     public int targets_reached = 0;
     public int targets_lost = 0;
     public int total_runs = 0;
-    public int accuracy;
+    public float accuracy;
+    public string run_ID;
 
         [Header("VARIABLES")]
     public Transform Target;
     private Rigidbody rb;
     public float jumpForce = 5f;
-    public float moveSpeed = 3f;
+    public float moveSpeed = 3f; // Horizontal movement speed during jump
     public float fallforce = 1f;
     private bool isGrounded;
     public float jumpcooldown = 6;
-
     float currentPos;
 private void  Start()
 {
-    float x  = transform.localPosition.x - Target.position.x;
-    float y  = transform.localPosition.y - Target.position.y;
-    currentPos = Mathf.Sqrt(x + y);
+    float x  = Target.localPosition.x - transform.localPosition.x;
+    float y  = Target.localPosition.y - transform.position.y;
+    currentPos = Mathf.Sqrt((x*x) + (y*y));
+    run_ID = GetRunIDFromArgs();
 }
 
 void Update()
@@ -35,10 +38,22 @@ void Update()
     accuracy = targets_reached / total_runs;
     }
 }
+ private string GetRunIDFromArgs()
+    {
+        string[] args = Environment.GetCommandLineArgs();
+        for (int i = 0; i < args.Length; i++)
+        {
+            if (args[i] == "--run-id" && i + 1 < args.Length)
+            {
+                return args[i + 1];
+            }
+        }
+        return null;
+    }
     public override void Initialize()
     {
-        Target.transform.localPosition = new Vector3(Random.Range(-2.15f, 2.0f), 0.41f, 6);
-        transform.localPosition = new Vector3(Random.Range(-1.76f, 1.70f), 1, -0.8f);
+        Target.transform.localPosition = new Vector3(UnityEngine.Random.Range(-2.15f, 2.0f), 0.41f, 6);
+        transform.localPosition = new Vector3(UnityEngine.Random.Range(-1.76f, 1.70f), 1, -0.8f);
         rb = GetComponent<Rigidbody>();
         lastJumpTime -= jumpcooldown;
     }
@@ -69,9 +84,9 @@ void Update()
         // }
         if(currentPos > distanceToTarget) {
             SetReward(0.1f);
-            float x = transform.localPosition.x - Target.position.x;
-            float y  = transform.localPosition.y - Target.position.y;
-            currentPos = Mathf.Sqrt(x + y);
+            float x  = Target.localPosition.x - transform.localPosition.x;
+            float y  = Target.localPosition.y - transform.position.y;
+            currentPos = Mathf.Sqrt((x*x) + (y*y));
         }
         
 
